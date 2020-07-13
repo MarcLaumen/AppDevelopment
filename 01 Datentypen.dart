f6605df3bef3ffde86006b4a458d5dc21eb05c6b
MarcLaumen <68186820+MarcLaumen@users.noreply.github.com>
2020-07-13 14:44:03 +0200

    Create 01 Datentypen.dart

---
+++ 01 Datentypen.dart
@@ -0,0 +1,18 @@
+main()
+{
+  int i=1;
+  int n=3;
+  
+  print(i);
+  print(n);
+  
+  double o=1.2;
+  double l=3.5;
+  
+  print(o);
+  print(l);
+  
+  String a="Hallihallo";
+  
+  print(a);
+}
