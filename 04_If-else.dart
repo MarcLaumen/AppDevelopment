ea125a6baddf35470e444718b3fe85ad1485a3fe
MarcLaumen <68186820+MarcLaumen@users.noreply.github.com>
2020-07-14 11:18:53 +0200

    Create 04_If-else.dart

---
+++ 04_If-else.dart
@@ -0,0 +1,14 @@
+main()
+{
+  int zahlengrenze=10;
+  if (zahlengrenze>10)
+  {
+    print("Juhu");
+  }
+  else
+  {
+    print("Oh nein");
+    
+        print(zahlengrenze>10?"Zahlengrenze > 10":"Zahlengrenze < 10");
+  }
+}
